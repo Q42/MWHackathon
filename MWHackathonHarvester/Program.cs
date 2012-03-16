a26723220afc8d4bb35e3145e392d4b4e9bbcd57
lukasvan3l <lukas@q42.nl>
2012-03-16 16:37:05 +0100

    db er achter

--- MWHackathonHarvester/Program.cs
+++ MWHackathonHarvester/Program.cs
@@ -2,13 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using log4net;
+using MWHackathonHarvester.Services;
 
 namespace MWHackathonHarvester
 {
   class Program
   {
+    private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
     static void Main(string[] args)
     {
+      log4net.Config.XmlConfigurator.Configure();
+      log.Info("Starting harvest");
+
+      var db = new DatabaseService();
+
+      var feed = db.GetFeed("Rijksmuseum Amsterdam");
+
+      Console.WriteLine("Done!");
+      Console.ReadKey(true);
     }
   }
 }
---
+++ MWHackathonHarvester/Services/DatabaseService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MWHackathonHarvester.Models;
+using log4net;
+
+namespace MWHackathonHarvester.Services
+{
+  public class DatabaseService
+  {
+
+    private static readonly ILog log = LogManager.GetLogger(typeof(DatabaseService));
+
+    public List<Feed> GetAllFeeds()
+    {
+      using (var db = new DatabaseDataContext())
+        return db.Feeds.ToList();
+    }
+
+    public List<Entry> GetAllEntries(int FeedId)
+    {
+      using (var db = new DatabaseDataContext())
+        return db.Entries.Where(e => e.feed_id == FeedId).ToList();
+    }
+
+    public Feed GetFeed(string name)
+    {
+      using (var db = new DatabaseDataContext())
+      {
+        var feed = db.Feeds.SingleOrDefault(f => f.name == name);
+        if (feed == null)
+        {
+          log.Warn("Creating new feed: " + name);
+          feed = new Feed() { name = name };
+          db.Feeds.InsertOnSubmit(feed);
+          db.SubmitChanges();
+        }
+        return feed;
+      }
+    }
+
+
+  }
+}
